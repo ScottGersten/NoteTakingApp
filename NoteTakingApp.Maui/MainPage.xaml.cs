781e17e5799b1164df786c919183c8ab27362e1d
ScottGersten <130173316+ScottGersten@users.noreply.github.com>
2024-08-10 12:19:12 -0400

    Experiment 1, XAML

--- NoteTakingApp.Maui/MainPage.xaml.cs
+++ NoteTakingApp.Maui/MainPage.xaml.cs
@@ -1,25 +1,50 @@
-﻿namespace NoteTakingApp.Maui
+﻿namespace NoteTakingApp.Maui;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    int count = 0;
+
+    public MainPage()
     {
-        int count = 0;
-
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        InitializeComponent();
     }
 
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+
+    private async void MyButtonClicked(object sender, EventArgs e)
+    {
+        /*var client = new HttpClient();
+        var result = await client.GetStringAsync("google.com");
+        File.WriteAllText("text.txt",result);
+        await File.WriteAllTextAsync("text.txt", result);*/
+
+        if (!string.IsNullOrEmpty(NoteText.Text))
+        {
+            string note_text = NoteText.Text;
+        }
+        else
+        {
+            var k = await DisplayAlert("Alert", "Would you like to save?", "Yes", "No");
+
+
+
+
+        }
+
+    }
+
+    private void NoteText_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        WatermarkLabel.IsVisible = string.IsNullOrEmpty(NoteText.Text);
+    }
 }
