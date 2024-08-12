564e7bf52222042a86d3033957eb0276e471165e
ScottGersten <130173316+ScottGersten@users.noreply.github.com>
2024-08-12 19:34:04 -0400

    Starting logic for main page

--- NoteTakingApp.Maui/MainPage.xaml.cs
+++ NoteTakingApp.Maui/MainPage.xaml.cs
@@ -1,16 +1,23 @@
-﻿namespace NoteTakingApp.Maui;
+﻿using Microsoft.Maui.Controls;
+using NoteTakingApp.Maui;
+using System.Collections.ObjectModel;
+
+namespace NoteTakingApp.Maui;
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
+    ObservableCollection<Note> notes;
 
     public MainPage()
     {
         InitializeComponent();
+        notes = new ObservableCollection<Note>();
+        NotesCollectionView.ItemsSource = notes;
     }
 
     private void TitleEditor_TextChanged(object sender, TextChangedEventArgs e)
     {
+        
 
     }
 
@@ -21,7 +28,22 @@ public partial class MainPage : ContentPage
 
     private void AddNoteButton_Clicked(object sender, EventArgs e)
     {
+        string title = TitleEditor.Text;
+        string text = NoteEditor.Text;
 
+        if (!string.IsNullOrEmpty(title) && !string.IsNullOrEmpty(text))
+        {
+            Note newNote = new Note(title, text);
+            notes.Add(newNote);
+
+            TitleEditor.Text = string.Empty;
+            NoteEditor.Text = string.Empty;
+        }
+
+        else
+        {
+            DisplayAlert("Error", "Please enter a note title and body", "OK");
+        }
     }
 
     private void ClearNoteButton_Clicked(object sender, EventArgs e)
@@ -39,42 +61,5 @@ public partial class MainPage : ContentPage
 
     }
 
-    /*private void OnCounterClicked(object sender, EventArgs e)
-    {
-        count++;
-
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
-    }
-
-    private async void MyButtonClicked(object sender, EventArgs e)
-    {
-        *//*var client = new HttpClient();
-        var result = await client.GetStringAsync("google.com");
-        File.WriteAllText("text.txt",result);
-        await File.WriteAllTextAsync("text.txt", result);*//*
-
-        if (!string.IsNullOrEmpty(NoteText.Text))
-        {
-            string note_text = NoteText.Text;
-        }
-        else
-        {
-            var k = await DisplayAlert("Alert", "Would you like to save?", "Yes", "No");
-
-
-
-
-        }
-
-    }
-
-    private void NoteText_TextChanged(object sender, TextChangedEventArgs e)
-    {
-        WatermarkLabel.IsVisible = string.IsNullOrEmpty(NoteText.Text);
-    }*/
+    
 }
